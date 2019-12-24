ddd206f3532b590e44125237815ab4ed915b625e
bughuntid <57358768+bughuntid@users.noreply.github.com>
2019-12-25 04:11:37 +0700

    Add files via upload

--- detect.py
+++ detect.py
@@ -240,7 +240,7 @@ if __name__ ==  '__main__':
             im_id = i*batch_size + im_num
             objs = [classes[int(x[-1])] for x in output if int(x[0]) == im_id]
             print("{0:20s} predicted in {1:6.3f} seconds".format(image.split("/")[-1], (end - start)/batch_size))
-            print("{0:20s} {1:s}".format("Objects Detected:", " ".join(objs))\n)
+            print("{0:20s} {1:s}".format("Objects Detected:", "\n ".join(objs)))
             print("----------------------------------------------------------")
         i += 1
 
